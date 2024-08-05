c7ddcea2f3d60d42669c17d622699c140d756445
varjain <varjain@redhat.com>
2024-08-05 12:18:57 +0530

    make the files executable

--- jenkins/import-central-artifacts-to-brew.sh
+++ jenkins/import-central-artifacts-to-brew.sh
@@ -2,6 +2,8 @@
 input=$1
 # this script needs to be run inside the below docker/podman container
 # podman run -it --rm images.paas.redhat.com/dbruscin/rh-utility-scripts
+# docker pull images.paas.redhat.com/dbruscin/rh-utility-scripts
+# docker run --name mycontainer -d images.paas.redhat.com/dbruscin/rh-utility-scripts
 while IFS= read -r line; do
     	./utility-scripts/mead/mead-download-and-import -v -t quarkus-3.8-maven-imports ${line} || true
         sleep 5
